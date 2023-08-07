d1dcffd2d926fb66989d41a4389287969c38409a
Bernardo Duarte de Miranda <112958256+bernardodm@users.noreply.github.com>
2023-08-07 15:32:18 -0300

    update: addpage update

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -40,7 +40,7 @@ class Home extends StatelessWidget {
                       ),
                     ),
                   ),
-                  CustomActivity()
+                  CustomActivity(),
                 ],
               ),
             ),
--- lib/shared/widgets/custom_activity.dart
+++ lib/shared/widgets/custom_activity.dart
@@ -1,42 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/add/widgets/custom_form_add.dart';
 
-String nameForm = '';
-String valueForm = '';
-String dataForm = '';
-String descriptionForm = '';
-
-class CustomActivity extends StatelessWidget {
+class CustomActivity extends StatefulWidget {
   const CustomActivity({super.key});
 
   @override
+  State<CustomActivity> createState() => _CustomActivityState();
+}
+
+class _CustomActivityState extends State<CustomActivity> {
+  final List<Activity> _activity = [];
+  void newActivity(Activity item) {
+    setState(() {
+      _activity.add(item);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: 70,
-          width: 400,
-          decoration: BoxDecoration(
-            border: Border.all(color: const Color.fromARGB(59, 0, 0, 0)),
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: const [
-              BoxShadow(color: Colors.grey, blurRadius: 10),
-            ],
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                nameForm,
+    return Expanded(
+      child: ListView.builder(
+        itemCount: _activity.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 70,
+              width: 400,
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromARGB(59, 0, 0, 0)),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(color: Colors.grey, blurRadius: 10),
+                ],
               ),
-              Text(
-                valueForm,
-                style: const TextStyle(color: Colors.red),
-              )
-            ],
-          ),
-        ),
+              child: ListTile(
+                title: Text(_activity[index].nameForm),
+                subtitle: Text(_activity[index].descriptionForm),
+                trailing: Text(
+                    'R\$ ${_activity[index].valueForm.toStringAsFixed(2)}'),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
