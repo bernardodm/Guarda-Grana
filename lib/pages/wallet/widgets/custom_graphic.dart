35429d8f804f3a73242e9235c26220ca84f820ce
Bernardo Duarte de Miranda <bernardo.duartemiranda@gmail.com>
2023-10-09 16:12:14 -0300

    Update custom_graphic.dart

--- lib/pages/wallet/widgets/custom_graphic.dart
+++ lib/pages/wallet/widgets/custom_graphic.dart
@@ -1,22 +1,43 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/shared/themes/colors.dart';
+import 'package:my_app/pages/add/widgets/custom_form_add.dart';
 
 class CustomGraphic extends StatelessWidget {
   const CustomGraphic({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 250,
-        width: MediaQuery.of(context).size.width - 50,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: CustomColors.color1,
-        ),
-        child: const Text('Aqui vai ficar o gráfico'),
-      ),
-    );
+    return SizedBox(
+        width: 400,
+        height: 300,
+        child: LineChart(
+          LineChartData(
+            gridData: const FlGridData(show: false),
+            titlesData: const FlTitlesData(show: true),
+            borderData: FlBorderData(
+              show: true,
+              border: Border.all(
+                color: const Color(0xff37434d),
+                width: 1,
+              ),
+            ),
+            minX: 0,
+            maxX: activityValue.length.toDouble() - 1,
+            minY: 0,
+            maxY: 50,
+            lineBarsData: [
+              LineChartBarData(
+                spots: activityValue.asMap().entries.map((entry) {
+                  return FlSpot(entry.key.toDouble(), entry.value);
+                }).toList(),
+                isCurved: true,
+                dotData:
+                    const FlDotData(show: false), // Mostrar pontos no gráfico
+                belowBarData:
+                    BarAreaData(show: false), // Área abaixo do gráfico
+              ),
+            ],
+          ),
+        ));
   }
 }
